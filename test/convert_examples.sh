7c895d001e623a2c5aceb18664e9fd94532b1651
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2011-10-03 17:29:20 +0200

    * Added third party OpenAir files. * Improved script for generating all the files.

--- test/convert_examples.sh
+++ test/convert_examples.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-# Some complete maps.
-../src/oa2pm ../openair_examples/AirspaceBelgium2.txt > output/AirspaceBelgium2.mp
-../src/oa2pm ../openair_examples/WEEK_BE_FRONT.txt > output/WEEK_BE_FRONT.mp
-../src/oa2pm ../openair_examples/winpilot_example.txt > output/winpilot_example.mp
+echo -n "Converting test OpenAir files..."
+for file in `ls ../openair/tests/*.txt`
+do
+  ../src/oa2pm $file > ${file%%.txt}.mp;
+done
+echo " done."
 
-# Some simple stand-alone examples with only 1 or 2 airspaces for debugging.
-../src/oa2pm ../openair_examples/two_arcs.txt > output/two_arcs.mp
-../src/oa2pm ../openair_examples/negative_arc.txt > output/negative_arc.mp
-../src/oa2pm ../openair_examples/only_circle.txt > output/only_circle.mp
-../src/oa2pm ../openair_examples/liege_ctr.txt > output/liege_ctr.mp
-../src/oa2pm ../openair_examples/DC_default.txt > ../openair_examples/DC_default.mp
-../src/oa2pm ../openair_examples/DA_positive.txt > ../openair_examples/DA_positive.mp
-../src/oa2pm ../openair_examples/DA_positive2.txt > ../openair_examples/DA_positive2.mp
-../src/oa2pm ../openair_examples/DA_negative.txt > ../openair_examples/DA_negative.mp
-../src/oa2pm ../openair_examples/DB_positive.txt > ../openair_examples/DB_positive.mp
+echo -n "Combining OpenAir files for the Belgian airspace to one map..."
+cat ../openair/belgium/*.txt > ../openair/belgium/belgium.TXT
+../src/oa2pm ../openair/belgium/belgium.TXT > ../openair/belgium/belgium.mp
+echo " done."
+
+echo -n "Converting third party OpenAir files..."
+for file in `ls ../openair/third_party/*.txt`
+do
+  ../src/oa2pm $file > ${file%%.txt}.mp;
+done
+echo " done."
