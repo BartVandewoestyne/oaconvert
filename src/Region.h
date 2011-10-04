82f580a0f616254660e3efef2d2d5432315426fe
Yves Frederix <yves.frederix@gmail.com>
2011-10-04 23:07:24 +0200

    Added Region::discretize()

--- src/Region.h
+++ src/Region.h
@@ -2,12 +2,14 @@
 #define REGION_H
 
 #include <ostream>
+#include <vector>
+
+#include "Coordinate.h"
 
 //////////////////////////////////////////////////////////////////////////////////////////
 // Forward declarations
 //////////////////////////////////////////////////////////////////////////////////////////
 class OutputState;
-
 
 //////////////////////////////////////////////////////////////////////////////////////////
 // Interface Region
@@ -18,6 +20,8 @@ class Region
     virtual ~Region() {};
 
     virtual void write( std::ostream& stream, const OutputState* outputstate ) const = 0;
+
+    virtual void discretize( std::vector<Coordinate>& coords, double resolution ) const = 0;
 
     virtual std::ostream& print( std::ostream &stream ) = 0;
   };
