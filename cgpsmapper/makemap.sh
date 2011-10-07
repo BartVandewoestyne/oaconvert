2de003c6b6ab37c3b57aca85c0b1e54286b73b5f
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2011-10-08 00:35:13 +0200

    * Made script more convenient.

--- cgpsmapper/makemap.sh
+++ cgpsmapper/makemap.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-# Copy our generated Polish Files here.
-cp ../openair/belgium/belgium.mp .
+# Check if a Polish file has been specified.
+if [ $# -ne 1 ]
+then
+  echo "Usage: `basename $0` path/to/some_polish_file.mp"
+  exit $E_BADARGS
+fi
+
+# Remove old junk.
+rm -f *.reg *.img *.TDB
 
 # First, create an IMG file from the given Polish file.  With the 'ac' option
 # we make sure that the name of the file will be as defined by the ID key
 # in the header of the Polish file.  The -l option is just more elaborate
 # error info.
-cgpsmapper -l ac belgium.mp
+cgpsmapper -l ac $1
 
 # Secondly, use our preview control file (mypreview.mp) to generate all the
 # necessary files (.TDB, .REG, .IMG) so that we can load all the stuff as
