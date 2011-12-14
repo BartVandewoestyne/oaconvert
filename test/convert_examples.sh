68654acb39e2ce0c7db6a5837fe11680e0facd53
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2011-12-15 00:25:16 +0100

    * Removed deprecated part.

--- test/convert_examples.sh
+++ test/convert_examples.sh
@@ -7,11 +7,6 @@
 #done
 #echo " done."
 
-echo -n "Combining OpenAir files for the Belgian airspace to one map..."
-cat ../openair/belgium/*.txt > ../openair/belgium/belgium.TXT
-../src/oaconvert -o ../openair/belgium/belgium.mp ../openair/belgium/belgium.TXT
-echo " done."
-
 echo "Converting third party OpenAir files..."
 for file in `ls ../openair/third_party/winpilot/*.txt ../openair/third_party/wim_verhoeve/*.txt ../openair/third_party/lvzc/*.txt`
 do
