4e7c91d3bece1970ad3654374c77d429b73d69f3
Markus Eich <markus.eich@qut.edu.au>
2015-09-29 12:07:32 +1000

    added save readline for cross plattform parsing (dos/unix)

--- src/oaconvert.cpp
+++ src/oaconvert.cpp
@@ -29,6 +29,40 @@
 
 using namespace std;
 
+static std::istream& safeGetline(std::istream& is, std::string& t)
+{
+    t.clear();
+
+    // The characters in the stream are read one-by-one using a std::streambuf.
+    // That is faster than reading them one-by-one using the std::istream.
+    // Code that uses streambuf this way must be guarded by a sentry object.
+    // The sentry object performs various tasks,
+    // such as thread synchronization and updating the stream state.
+
+    std::istream::sentry se(is, true);
+    std::streambuf* sb = is.rdbuf();
+
+    for(;;) {
+        int c = sb->sbumpc();
+        switch (c) {
+        case '\n':
+            return is;
+        case '\r':
+            if(sb->sgetc() == '\n')
+                sb->sbumpc();
+            return is;
+        case EOF:
+            // Also handle the case when the last line has no line ending
+            if(t.empty())
+                is.setstate(std::ios::eofbit);
+            return is;
+        default:
+            t += static_cast<char> (c);
+        }
+    }
+}
+
+
 int main (int argc, char* argv[])
 {
 
@@ -152,7 +186,7 @@ int main (int argc, char* argv[])
     {
         while ( inStream.good() )
         {
-            getline(inStream, line);
+            safeGetline(inStream, line);
             p->handleLine(line);
         }
 
